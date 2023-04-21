d5260e3d51c1f721007b3aead19da285c0cb5422
Gerrit <gerrit.sonnabend@gmail.com>
2023-04-21 12:32:30 -0500

    first pass at removing things that I dont need

--- main.py
+++ main.py
@@ -1,25 +1,5 @@
 import openai_integration
-import page_parser
 
-MAX_CHAR_LENGTH_OUTREACH_TARGET = 1000
-MAX_CHAR_LENGTH_LINK_TARGET = 500
-
-
-# if over limit, keep a part at the beginning and at the end of the string. Usually intro and summary are where the article contains the most dense information.
-def truncate_input(string, limit):
-    if len(string) <= limit:
-        return string
-
-    beginning_ratio = 2 / 3
-    end_ratio = 1 / 3
-
-    beginning_length = int(limit * beginning_ratio)
-    end_length = limit - beginning_length
-
-    beginning = string[:beginning_length]
-    end = string[-end_length:]
-
-    return beginning + end
 
 def build_prompt(outreach_content, link_target_content):
     base_prompt = "I am pasting you two articles. The first, named OutreachTarget is the article of the website that we are contacting. The second, named LinkTarget is the article that we want to get a backlink from. It is possible that the original articles are too long for you to process. That is why we will preprocess the article if it is too long and only send you the beginning and the end of the articles. However, those parts of the article probably will contain the most dense information and provide you with sufficient context. Do not confuse those two articles with each other. Next I am sending the articles."
