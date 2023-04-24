80aaaf8e893e05fb8147ca0c00522aeba4e74d28
Gerrit <gerrit.sonnabend@gmail.com>
2023-04-24 12:37:03 -0500

    added a template for the input form

---
+++ app.py
@@ -0,0 +1,49 @@
+import logging
+from logging.handlers import RotatingFileHandler
+
+from flask import Flask, render_template, request, redirect, url_for
+from flask_bootstrap import Bootstrap
+
+app = Flask(__name__)
+Bootstrap(app)
+
+def setup_logging():
+    log_level = logging.DEBUG if app.debug else logging.INFO
+
+    # Create a custom logger for the application
+    logger = logging.getLogger("myapp")
+    logger.setLevel(log_level)
+
+    formatter = logging.Formatter(
+        "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+    )
+
+    # Log to a file with a rotating handler
+    file_handler = RotatingFileHandler("logs/app.log", maxBytes=1024 * 1024, backupCount=10)
+    file_handler.setLevel(log_level)
+    file_handler.setFormatter(formatter)
+    logger.addHandler(file_handler)
+
+    # Log to the console
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(log_level)
+    console_handler.setFormatter(formatter)
+    logger.addHandler(console_handler)
+
+# Set up logging
+setup_logging()
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return render_template('recipe_generate_form.html')
+
+@app.route('/submit_form', methods=['POST'])
+def submit_form():
+    dropdown = request.form.get('dropdown')
+    text_input = request.form.get('text_input')
+    print(f'Dropdown: {dropdown}, Text Input: {text_input}')
+    return redirect(url_for('index'))
+
+if __name__ == '__main__':
+    app.run(debug=True)
